d0141abc1919b85bdfd70adbe17e47ce1a42edb3
Diego Cravo Teixeira <cravo.diego@gmail.com>
2020-09-17 09:15:19 -0300

    Teste melhorado

--- src/Test/ExameTest.java
+++ src/Test/ExameTest.java
@@ -2,19 +2,27 @@ package Test;
 
 import Entities.*;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class ExameTest {
+
+    @Rule
+    public ErrorCollector error = new ErrorCollector();
 
     @Test
     public void obterDiagnostico() {
         Exame exame = new Exame(1, "Sangue", 126);
 
-        assertEquals("Diabetes", exame.obterDiagnostico());
+        error.checkThat(exame.obterDiagnostico(), is("Diabetes"));
         exame.setNivelGlicose(98);
-        assertEquals("Normal", exame.obterDiagnostico());
+        error.checkThat(exame.obterDiagnostico(), is("Normal"));
         exame.setNivelGlicose(125);
-        assertEquals("Pré-Diabetes", exame.obterDiagnostico());
+        error.checkThat(exame.obterDiagnostico(), CoreMatchers.is("Pré-Diabetes"));
     }
 }
